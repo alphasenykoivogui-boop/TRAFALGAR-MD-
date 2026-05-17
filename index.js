ee2702cb7d8620e972edad3f0546ebe002b60f9b
TRAFALGAR MD <alphasenykoivogui@gmail.com>
2026-05-17 14:15:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const { default: makeWASocket } = require("@whiskeysockets/baileys")
 
-async function startBot() {
-    const sock = makeWASocket({})
-    
-    console.log("TRAFALGAR MD démarré")
-}
-
-startBot()
